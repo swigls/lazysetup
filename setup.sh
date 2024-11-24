cbeafb1faf8df208b248b6783f392fabfc36890b
Hyeonseung Lee <swigls1@gmail.com>
2024-11-24 03:21:15 +0000

    setup no argparser

--- setup.sh
+++ setup.sh
@@ -1,29 +1,5 @@
 source fn.sh || exit 1
 
-# Argument parser
-programname=$0
-function usage {
-    echo ""
-    echo "Setup development environments by executing a single script."
-    echo ""
-    echo "usage: $programname --remove_existing bool"
-    echo ""
-    echo "  --remove_existing bool  Whether to remove existing files to setup."
-    echo "                          (example: y/Y to yes, n/N to no)"
-    echo ""
-}
-while [ $# -gt 0 ]; do
-  if [[ $1 == "--help" ]]; then
-    usage
-    return 0
-  elif [[ $1 == "--"* ]]; then
-    v="${1/--/}"
-    declare "$v"="$2"
-    shift
-  fi
-  shift
-done
-
 password_check
 maybe_ask_preset_envvar remove_existing
 
