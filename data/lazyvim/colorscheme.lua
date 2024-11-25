90b488d07a56ace888f75c7d518e9e3dcc2a5294
Hyeonseung Lee <swigls1@gmail.com>
2024-11-25 17:23:35 +0000

    nvim colorscheme to gruvbox-material

--- data/lazyvim/colorscheme.lua
+++ data/lazyvim/colorscheme.lua
@@ -1,17 +1,9 @@
 return {
-  -- add gruvbox
-  { "sainnhe/gruvbox-material" },
-  { "projekt0n/github-nvim-theme" },
-  { "folke/tokyonight.nvim" },
-
-  -- Configure LazyVim to load gruvbox
   {
-    "LazyVim/LazyVim",
+     "sainnhe/gruvbox-material",
      lazy = false,
      priority = 1000,
      config = function()
-       -- Optionally configure and load the colorscheme
-       -- directly inside the plugin declaration.
        vim.g.gruvbox_material_enable_italic = true
        vim.cmd.colorscheme('gruvbox-material')
      end
