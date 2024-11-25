0ee8853c17e76ef7940b0b725ead8dd5caedfff3
Hyeonseung Lee <swigls1@gmail.com>
2024-11-25 08:07:32 +0000

    make it able to push by configuring

--- configure/git.sh
+++ configure/git.sh
@@ -1,4 +1,12 @@
 source fn.sh || exit 1
 
-git config --global user.email "swigls1@gmail.com"
-git config --global user.name "Hyeonseung Lee"
+password_check
+
+
+email=$(decrypt $(datadir)/git/gloabl_user.email)
+git config --global user.email "$email"
+
+name=$(decrypt $(datadir)/git/gloabl_user.name)
+git config --global user.name "$name"
+
+git remote set-url origin "git@github.com:swigls/lazysetup" 
