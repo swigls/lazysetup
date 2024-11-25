524fd1bb0a95dad6961991b57519cb9e8aa40f5f
Hyeonseung Lee <swigls1@gmail.com>
2024-11-25 09:43:07 +0000

    typo fix

--- configure/git.sh
+++ configure/git.sh
@@ -3,10 +3,10 @@ source fn.sh || exit 1
 password_check
 
 
-email=$(decrypt $(datadir)/git/gloabl_user.email)
+email=$(decrypt $(datadir)/git/global_user.email)
 git config --global user.email "$email"
 
-name=$(decrypt $(datadir)/git/gloabl_user.name)
+name=$(decrypt $(datadir)/git/global_user.name)
 git config --global user.name "$name"
 
 git remote set-url origin "git@github.com:swigls/lazysetup" 
