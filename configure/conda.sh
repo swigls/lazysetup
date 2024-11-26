03fe14bd0775fe30145a4d747f4d43aae7921461
Hyeonseung Lee <swigls1@gmail.com>
2024-11-26 08:34:22 +0000

    error fix: conda

--- configure/conda.sh
+++ configure/conda.sh
@@ -1,25 +1,17 @@
 source fn.sh || exit 1
 
-env_exist_fn="function env_exist {
-  interested_env=\$1
-  conda env list | while read line; do
-    env_path=\$(echo \"\$line\" | awk '{print \$NF}')
-    env_basename=\$(basename \"\$env_path\")
-    [ -z \$env_basename ] && continue
-    [ \"\$env_basename\" == \"\$interested_env\" ] && echo \"exist\"; break
-  done
-}"
-act_fn="function act {
-  envname=\$(basename \$(git rev-parse --show-toplevel))
-  if [ -z \$(env_exist \$envname) ]; then
-    read -p \"\$envname not existing, thus creating it. Python version? \" python_version
-    conda create -n \$envname python==\$python_version pip setuptools
+act_fn='
+function act {
+  envname=$(basename $(git rev-parse --show-toplevel))
+  not_exist=
+  conda activate $envname || not_exist=1
+  if [[ $not_exist ]]; then
+    read -p "No \"$envname\" environment, thus creating it. Which Python version? " python_version
+    conda create -n $envname python==$python_version pip setuptools
   fi
-  conda activate \$(basename \$(git rev-parse --show-toplevel))
-}"
-deact_fn="function deact {
+}'
+deact_fn='function deact {
   conda deactivate
-}"
-rc_append ~/.bashrc "$env_exist_fn"
+}'
 rc_append ~/.bashrc "$act_fn"
 rc_append ~/.bashrc "$deact_fn"
