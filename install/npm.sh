da757ba786ce35bc2ea806941496998424410edc
Hyeonseung <swigls1@gmail.com>
2024-12-03 08:24:50 +0000

    npm install

---
+++ install/npm.sh
@@ -0,0 +1,14 @@
+source fn.sh || exit 1
+
+# nvm
+remove_or_exit $(installdir)/xdg_base/.config/nvm
+rc_append_line $(installdir)/.bashrc 'export NVM_DIR="'$(installdir)'/xdg_base/.config/nvm"'
+rc_append_line $(installdir)/.bashrc '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"'                   # This loads nvm
+rc_append_line $(installdir)/.bashrc '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"' # This loads nvm bash_completion
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh |
+  PROFILE=/dev/null bash
+
+# node & npm
+source $(installdir)/.bashrc
+nvm install --lts
