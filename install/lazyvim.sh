5c4dabd7d4d9967fc68e555799380031fbfd964d
Hyeonseung <swigls1@gmail.com>
2024-12-04 04:46:33 +0000

    extras added

--- install/lazyvim.sh
+++ install/lazyvim.sh
@@ -1,13 +1,24 @@
 source fn.sh || exit 1
 
-remove_or_exit $(installdir)/xdg_base
+remove_or_exit $(installdir)/xdg_base/.config/nvim
+remove_or_exit $(installdir)/xdg_base/.local/share/nvim
+remove_or_exit $(installdir)/xdg_base/.local/state/nvim
+remove_or_exit $(installdir)/xdg_base/.cache/nvim
 
 git clone https://github.com/LazyVim/starter $(installdir)/xdg_base/.config/nvim --depth 1
 rm -rf $(installdir)/xdg_base/.config/nvim/.git
 
-# Colorscheme
-remove_or_exit $(installdir)/xdg_base/.config/nvim/lua/colorscheme.lua
-cp data/lazyvim/colorscheme.lua $(installdir)/xdg_base/.config/nvim/lua/plugins/
+# Plugins
+for filepath in data/lazyvim/plugins/*; do
+  filename=$(basename $filepath)
+  cp -f $filepath $(installdir)/xdg_base/.config/nvim/lua/plugins/
+done
+
+# Configs
+for filepath in data/lazyvim/config/*; do
+  filename=$(basename $filepath)
+  cp -f $filepath $(installdir)/xdg_base/.config/nvim/lua/config/
+done
 
 # Interactive installation of LazyVim plugins
 source $(installdir)/.bashrc
