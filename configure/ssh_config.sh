9014de35fa97b8d8c450a453aa7023cf33b6410f
Hyeonseung <swigls1@gmail.com>
2024-12-14 12:36:25 +0000

    public key in authorized_keys

--- configure/ssh_config.sh
+++ configure/ssh_config.sh
@@ -14,6 +14,7 @@ decrypt $(datadir)/ssh/id_ed25519 >$(installdir)/.ssh/id_ed25519
 chmod 400 $(installdir)/.ssh/id_ed25519
 
 decrypt $(datadir)/ssh/id_ed25519.pub >$(installdir)/.ssh/id_ed25519.pub
+decrypt $(datadir)/ssh/id_ed25519.pub >$(installdir)/.ssh/authorized_keys
 
 rc_append_line $(installdir)/.bashrc "export SSH_CONFIG_FILE=$(installdir)/.ssh/config"
 rc_append_line $(installdir)/.bashrc "export GIT_SSH_COMMAND='ssh -F $(installdir)/.ssh/config'"
