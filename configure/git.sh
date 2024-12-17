9163377472c61955eb75b8d92f14058c89de875a
Password <Password>
2024-12-17 07:36:26 +0000

    git.sh nitfix: double quotes

--- configure/git.sh
+++ configure/git.sh
@@ -4,9 +4,9 @@ source ~/.bashrc
 git_config_global pull.rebase false
 
 email=$(decrypt data/git/global_user.email)
-git_config_global user.email $email
+git_config_global user.email "$email"
 
 name=$(decrypt data/git/global_user.name)
-git_config_global user.name $name
+git_config_global user.name "$name"
 
 git_remote_set_url
