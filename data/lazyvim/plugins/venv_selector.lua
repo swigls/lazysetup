43663f014773de36d8686c5ec626745cdf22ef3a
Hyeonseung <swigls1@gmail.com>
2025-01-17 02:50:37 +0000

    added venv_selctor.lua

---
+++ data/lazyvim/plugins/venv_selector.lua
@@ -0,0 +1,21 @@
+return {
+	"linux-cultist/venv-selector.nvim",
+  branch = "regexp", -- Use this branch for the new version
+  cmd = "VenvSelect",
+  enabled = function()
+    return LazyVim.has("telescope.nvim")
+  end,
+  opts = {
+    settings = {
+      options = {
+        notify_user_on_venv_activation = true,
+        anaconda_base_path = "~/.lazysetup/conda/",
+        anaconda_envs_path = "~/.lazysetup/conda/envs/",
+        search_workspace = false,
+      },
+    },
+  },
+  --  Call config for python files and load the cached venv automatically
+  ft = "python",
+  keys = { { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv", ft = "python" } },
+}
