1a987cac22132d685f44b98994f3e27e3360520b
Hyeonseung <swigls1@gmail.com>
2025-01-17 05:17:01 +0000

    added black and prettier

--- data/lazyvim/config/lazy.lua
+++ data/lazyvim/config/lazy.lua
@@ -25,6 +25,8 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.coding.yanky" },
 		{ import = "lazyvim.plugins.extras.editor.dial" },
 		{ import = "lazyvim.plugins.extras.editor.inc-rename" },
+		{ import = "lazyvim.plugins.extras.formatting.black" },
+		{ import = "lazyvim.plugins.extras.formatting.prettier" },
 		{ import = "lazyvim.plugins.extras.lang.markdown" },
 		{ import = "lazyvim.plugins.extras.lang.yaml" },
 		{ import = "lazyvim.plugins.extras.lang.json" },
