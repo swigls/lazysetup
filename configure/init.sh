382cffa1bd87ea10e1de1b9af507375cb61fb9c1
Hyeonseung Lee <swigls1@gmail.com>
2025-02-04 18:55:23 +0000

    include bin/ & fixed wrong source PATH

--- configure/init.sh
+++ configure/init.sh
@@ -3,6 +3,7 @@ source libsetup.sh || exit 1
 mkdir -p "$(lazysetup_root)"/bin
 
 rc_append_line ~/.bashrc "source $(lazysetup_root)/.bashrc" && touch "$(lazysetup_root)/.bashrc"
+rc_append_line "$(lazysetup_root)/.bashrc" "export PATH=$(lazysetup_root)/bin":\$PATH 
 rc_append_line ~/.inputrc '$include '"$(lazysetup_root)/.inputrc" && touch "$(lazysetup_root)/.inputrc"
 rc_append_line ~/.vimrc "source $(lazysetup_root)/.vimrc" && touch "$(lazysetup_root)/.vimrc"
 rc_append_line ~/.tmux.conf "source-file $(lazysetup_root)/.tmux.conf" && touch "$(lazysetup_root)/.tmux.conf"
