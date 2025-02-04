922886a0ca1e006db6812648820c6d0f24cd20fb
Hyeonseung Lee <swigls1@gmail.com>
2025-02-04 19:08:32 +0000

    no password check in uninstall

--- configure/git.sh
+++ configure/git.sh
@@ -1,6 +1,14 @@
 source ~/.bashrc
 source libsetup.sh || exit 1
 
+
+if [[ $UNINSTALL ]]; then
+  git_config_global pull.rebase ""
+  git_config_global user.email ""
+  git_config_global user.name ""
+  exit 0
+fi
+
 git_config_global pull.rebase false
 
 email=$(decrypt data/git/global_user.email)
