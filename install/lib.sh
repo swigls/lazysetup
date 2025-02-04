174d558ce222697021221220de6319a29dbbaf06
Hyeonseung Lee <swigls1@gmail.com>
2025-02-04 19:15:43 +0000

    typofix: exe.sh -> lib.sh

--- install/lib.sh
+++ install/lib.sh
@@ -2,5 +2,5 @@ source libsetup.sh || exit 1
 
 remove_or_exit "$(lazysetup_root)/lib.sh"
 
-cp exe.sh "$(lazysetup_root)/lib.sh"
+cp lib.sh "$(lazysetup_root)/lib.sh"
 rc_append_line "$(lazysetup_root)/.bashrc" "source $(lazysetup_root)/lib.sh"
