4d9b590aff8a0796a95da5fdee519c14f9fb9dd7
Hyeonseung Lee <swigls1@gmail.com>
2025-02-04 08:49:53 +0000

    lazyupdate

--- lib.sh
+++ lib.sh
@@ -21,17 +21,6 @@ function tb {
   tensorboard --logdir "$logdir" --host 0.0.0.0
 }
 
-# Installation-related functions
-}
-function lazyuninstall {
-  (
-    export UNINSTALL=1
-    bash configure/init.sh
-    bash configure/git.sh
-    rm -rf "$(installdir)"
-  )
-}
-
 # Conda-related functions
 function act {
   if [[ $1 ]]; then
@@ -49,4 +38,100 @@ function act {
 }
 function deact {
   conda deactivate
+}
+function current_conda_env {
+  echo "$(conda info --envs | grep '*' | awk '{print $1}')"
+}
+
+# Installation-related functions
+INSTALL_SCRIPTS=(
+  # init
+  "configure/init.sh"
+  # misc
+  "bash configure/no_beep.sh"
+  "bash configure/tmux.sh"
+  "bash configure/ssh.sh"
+  "bash install/exe.sh"
+  # git
+  "bash install/git.sh"
+  "bash configure/git.sh"
+  # conda
+  "bash install/conda.sh"
+  # requirements for lazyvim
+  "bash install/nvim.sh"
+  "bash install/lazygit.sh"
+  "bash install/ripgrep.sh"
+  "bash install/fd.sh"
+  "bash install/npm.sh"
+  # lazyvim
+  "bash install/lazyvim.sh"
+  "bash configure/lazyvim.sh"
+)
+function lazysourcerc {
+  # Re-source the bashrc while keeping the current conda environment
+  if command -v conda >/dev/null 2>&1; then
+    conda_env=$(current_conda_env)
+  else
+    conda_env=
+  fi
+  source ~/.bashrc
+  if [ "$conda_env" ]; then
+    conda activate "$conda_env"
+  fi
+}
+function lazyinstall_single {
+  script=$1
+  (
+    bash "$script"
+  )
+}
+function lazyupdate {
+  (
+    cd "$(lazysetup_root)" || exit 1
+    # If git is not installed, install it
+    if ! command -v git >/dev/null 2>&1; then
+      lazyinstall_single "install/git.sh"
+      lazysourcerc
+    fi
+
+    # cd to the git-cache dir for lazysetup
+    if [[ ! -d .gitcache ]]; then
+      # If no git cache found, install all
+      git clone https://github.com/swigls/lazysetup .gitcache || exit 1
+      changes=("${INSTALL_SCRIPTS[@]}")
+    fi
+    cd .gitcache || exit 1
+
+    # Check git remote update, and sync
+    [ -z "$changes" ] && changes=(git diff --name-only master origin/master)
+    if [[ -z "$changes" ]]; then
+      echo "No git changes detected"
+      exit 1
+    fi
+    git pull origin master
+
+    # Install the scripts that have changes
+    if [[ ! " ${changes[*]} " == " configure/lazyvim.sh " ]]; then
+      for change in "${changes[@]}"; do
+        if [[ "$change" == "data/lazyvim/"* ]]; then
+          changes+=("configure/lazyvim.sh")
+          break
+        fi
+      done
+    fi
+    for script in "${INSTALL_SCRIPTS[@]}"; do
+      if [[ " ${changes[*]} " == *" ${script} "* ]]; then
+        lazyinstall_single "$script"
+      fi
+    done
+  ) && lazysourcerc # Re-source the bashrc if changes are made
+}
+function lazyuninstall {
+  (
+    cd "$(lazysetup_root)" || exit 1
+    export UNINSTALL=1
+    bash configure/init.sh
+    bash configure/git.sh
+    rm -rf "$(lazysetup_root)"
+  )
 }
--- setup.sh
+++ setup.sh
@@ -1,28 +1,3 @@
 #!/bin/bash
-
-# init
-bash configure/init.sh
-
-# misc
-bash configure/no_beep.sh
-bash configure/tmux.sh
-bash configure/ssh.sh
-bash install/exe.sh
-
-# git
-bash install/git.sh
-bash configure/git.sh
-
-# conda
-bash install/conda.sh
-
-# requirements for lazyvim
-bash install/nvim.sh
-bash install/lazygit.sh
-bash install/ripgrep.sh
-bash install/fd.sh
-bash install/npm.sh
-
-# lazyvim
-bash install/lazyvim.sh
-bash configure/lazyvim.sh
+source libsetup.sh || exit 1
+lazyupdate
