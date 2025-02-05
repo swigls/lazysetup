bd47467e862a8a428f146dd8592c09d8307e5879
Hyeonseung Lee <swigls1@gmail.com>
2025-02-05 07:48:09 +0000

    fzf.sh: ppc64le -> amd64

--- install/fzf.sh
+++ install/fzf.sh
@@ -2,7 +2,7 @@ source libsetup.sh || exit 1
 
 remove_or_exit "$(lazysetup_root)/bin/fzf"
 
-curl_tar_and_extract https://github.com/junegunn/fzf/releases/download/v0.59.0/fzf-0.59.0-linux_ppc64le.tar.gz
+curl_tar_and_extract https://github.com/junegunn/fzf/releases/download/v0.59.0/fzf-0.59.0-linux_amd64.tar.gz
 mv ./fzf "$(lazysetup_root)/bin/fzf"
 
 rc_append_line "$(lazysetup_root)/.bashrc"  'eval "$(fzf --bash)"'
