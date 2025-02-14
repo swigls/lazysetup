bba2001fb9b181c0158ccb4267b1902a5d0209f2
Hyeonseung Lee <swigls1@gmail.com>
2025-02-14 17:23:14 +0000

    print out changes

--- lib.sh
+++ lib.sh
@@ -124,6 +124,7 @@ function lazyupdate {
     if [[ ! " ${changes[*]} " == " configure/lazyvim.sh " ]]; then
       changes+=("configure/lazyvim.sh")
     fi
+    echo "Changes detected: ${changes[*]}"
     for script in "${INSTALL_SCRIPTS[@]}"; do
       if [[ " ${changes[*]} " == *" ${script} "* ]]; then
         lazyinstall_single "$script"
