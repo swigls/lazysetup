3a669e5778644c77e7466cdc5a6ad89a51f82709
Hyeonseung Lee <swigls1@gmail.com>
2025-02-24 04:24:31 +0000

    also install libsetup.sh

--- install/lib.sh
+++ install/lib.sh
@@ -1,4 +1,5 @@
 source libsetup.sh || exit 1
 
 cp -f lib.sh "$(lazysetup_root)/lib.sh"
+cp -f libsetup.sh "$(lazysetup_root)/libsetup.sh"
 rc_append_line "$(lazysetup_root)/.bashrc" "source $(lazysetup_root)/lib.sh"
