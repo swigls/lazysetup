ca6792aa1318a6c3fbf5c8c7f28dd2a7aa1b7efe
Hyeonseung Lee <swigls1@gmail.com>
2025-04-25 06:37:13 +0000

    errfix

--- install/aihubshell.sh
+++ install/aihubshell.sh
@@ -4,5 +4,3 @@ curl -o "aihubshell" https://api.aihub.or.kr/api/aihubshell.do
 
 mv aihubshell "$(lazysetup_root)/bin/aihubshell"
 chmod +x "$(lazysetup_root)/bin/aihubshell"
-
-rc_append_line "$(lazysetup_root)/.bashrc" "source $(lazysetup_root)/bin/aihubshell"
