ccce04ae4ca2e4fae8620043a1a7e5cb107ac094
Hyeonseung Lee <swigls1@gmail.com>
2025-05-07 07:37:30 +0000

    constants: only append valide value to rc

--- configure/constants.sh
+++ configure/constants.sh
@@ -4,5 +4,9 @@ source libsetup.sh || exit 1
 for path in $(lazysetup_constants_root)/*; do
     constant_key=$(basename "$path")
     constant_value=$(decrypt "$path")
-    rc_append_line "$(lazysetup_root)"/.bashrc "export $constant_key=$constant_value"
+    if [[ -z $constant_value ]]; then
+        echo "Error: $path is empty or not a valid file."
+        continue
+    fi
+    rc_append_line "$(lazysetup_root)"/.bashrc "export $constant_key=\"$constant_value\""
 done
