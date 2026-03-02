980554912c0727999a373341730d28f8a93024dc
Hyeonseung Lee <swigls1@gmail.com>
2026-03-02 14:21:53 +0000

    zellij-attention

--- configure/zellij.sh
+++ configure/zellij.sh
@@ -3,3 +3,6 @@ source libsetup.sh || exit 1
 zellij_config_dir="$(lazysetup_root)/xdg_base/.config/zellij"
 mkdir -p "$zellij_config_dir"
 cp data/zellij/config.kdl "$zellij_config_dir"/config.kdl
+
+mkdir -p "$zellij_config_dir/layouts"
+cp data/zellij/default.kdl "$zellij_config_dir/layouts/default.kdl"
