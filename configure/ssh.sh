a9e9605b981bd127fe17e411192ad61fc9ed6f42
Hyeonseung Lee <swigls1@gmail.com>
2026-04-02 05:02:18 +0900

    Update zellij and remove gitui install

--- configure/ssh.sh
+++ configure/ssh.sh
@@ -14,9 +14,15 @@ Host github.com
 EOF
 fi
 
-decrypt data/ssh/id_ed25519 > ~/.ssh/id_ed25519
-chmod 400 ~/.ssh/id_ed25519
+tmp_private_key=$(mktemp)
+tmp_public_key=$(mktemp)
 
-decrypt data/ssh/id_ed25519.pub > ~/.ssh/id_ed25519.pub
+decrypt data/ssh/id_ed25519 > "$tmp_private_key"
+install -m 400 "$tmp_private_key" ~/.ssh/id_ed25519
+
+decrypt data/ssh/id_ed25519.pub > "$tmp_public_key"
+install -m 644 "$tmp_public_key" ~/.ssh/id_ed25519.pub
 cp -f ~/.ssh/id_ed25519.pub ~/.ssh/authorized_keys
-chmod 644 ~/.ssh/id_ed25519.pub ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
+
+rm -f "$tmp_private_key" "$tmp_public_key"
--- install/gitui.sh
+++ install/gitui.sh
@@ -1,7 +1,3 @@
 source libsetup.sh || exit 1
 
-GITUI_VERSION=$(curl -s https://api.github.com/repos/gitui-org/gitui/releases/latest | grep '"tag_name"' | cut -d'"' -f4)
-
-mkdir -p "$(lazysetup_bin_dir)"
-curl_tar_and_extract "https://github.com/gitui-org/gitui/releases/download/${GITUI_VERSION}/gitui-linux-x86_64.tar.gz"
-mv ./gitui "$(lazysetup_bin_dir)/gitui"
+rm -f "$(lazysetup_bin_dir)/gitui"
