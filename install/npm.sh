26c1baa6d72495123591b21b0cd4f7e6f35514e9
Hyeonseung Lee <swigls1@gmail.com>
2026-04-02 04:57:09 +0900

    errfix

--- install/npm.sh
+++ install/npm.sh
@@ -1,13 +1,13 @@
 source libsetup.sh || exit 1
 
-NVM_DIR="$(lazysetup_config_home)/nvm"
+NVM_DIR="$HOME/.nvm"
 rc_append_line ~/.bashrc "export NVM_DIR=\"$NVM_DIR\""
 rc_append_line ~/.bashrc '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"'
-rc_append_line ~/.bashrc '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"'
+# rc_append_line ~/.bashrc '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"'
 
-source ~/.bashrc
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh |
   PROFILE=/dev/null bash
 
-source ~/.bashrc
+export NVM_DIR=$HOME/.nvm
+[ -s $NVM_DIR/nvm.sh ] && \. "$NVM_DIR/nvm.sh"
 nvm install --lts
